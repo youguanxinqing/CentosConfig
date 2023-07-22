52dbd93ecae40e994d91f671faf8a612a20f9482
youguanxinqing <youguanxinqing@qq.com>
2023-07-22 20:10:03 +0800

    feat: add lock json

--- vim/nvchad/init.sh
+++ vim/nvchad/init.sh
@@ -1,2 +1,5 @@
 mv ~/.config/nvim/lua/custom ~/.config/nvim/lua/custom.bak
 ln -s `pwd`/custom ~/.config/nvim/lua/
+
+mv ~/.config/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json.bak
+ln -s `pwd`/custom/lazy-lock.json ~/.config/nvim/lazy-lock.json
