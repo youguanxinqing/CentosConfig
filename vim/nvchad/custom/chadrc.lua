d08e3caa8293fcf6ea5ff7365d44faf19ee14981
youguanxinqing <youguanxinqing@qq.com>
2023-07-22 19:47:17 +0800

    feat: add cursor pos record

--- vim/nvchad/custom/chadrc.lua
+++ vim/nvchad/custom/chadrc.lua
@@ -23,6 +23,9 @@ vim.cmd([[
 hi ColorColumn ctermbg=0 guibg=#242a32
 hi WinSeparator ctermbg=0 guifg=#555555
 hi GitSignsCurrentLineBlame ctermbg=0 guifg=#bbbbbb
+
+autocmd BufRead * autocmd FileType <buffer> ++once
+  \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
 ]])
 
 return M
