b63f659ff886cdb9a9e6fd8370c9080231f4c073
youguanxinqing <youguanxinqing@qq.com>
2023-09-03 18:42:12 +0800

    nvim: track nvchad's code

--- vim/nvchad/init.sh
+++ vim/nvchad/init.sh
@@ -1,5 +1,1 @@
-mv ~/.config/nvim/lua/custom ~/.config/nvim/lua/custom.bak
-ln -s `pwd`/custom ~/.config/nvim/lua/
-
-mv ~/.config/nvim/lazy-lock.json ~/.config/nvim/lazy-lock.json.bak
-ln -s `pwd`/custom/lazy-lock.json ~/.config/nvim/lazy-lock.json
+ln -s /home/guan/Public/Config/vim/nvchad/nvim ~/.config
